49d585169b7dd9c33c799a36611dab7886e158aa
DESKTOP-HBB590M\user <viceconti.federico@gmail.com>
2018-04-15 17:32:22 +0200

    Several versioning method

--- src/main/java/com/example/restfulwebservices/versioning/VersionController.java
+++ src/main/java/com/example/restfulwebservices/versioning/VersionController.java
@@ -9,12 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionController {
     @GetMapping("v1/some-person")
-    public PersonV1 PersonV1() {
+    public PersonV1 v1() {
         return new PersonV1("First Last");
     }
 
     @GetMapping("v2/some-person")
-    public PersonV2 PersonV2() {
+    public PersonV2 v2() {
+        return new PersonV2(new Name("First", "Last"));
+    }
+
+    @GetMapping(value = "/some-person/param", params = "version=1")
+    public PersonV1 param1() {
+        return new PersonV1("First Last");
+    }
+
+    @GetMapping(value = "/some-person/param", params = "version=2")
+    public PersonV2 param2() {
+        return new PersonV2(new Name("First", "Last"));
+    }
+
+    @GetMapping(value = "/some-person/header", headers = "X-API-VERSION=1")
+    public PersonV1 header1() {
+        return new PersonV1("First Last");
+    }
+
+    @GetMapping(value = "/some-person/header", headers = "X-API-VERSION=2")
+    public PersonV2 header2() {
         return new PersonV2(new Name("First", "Last"));
     }
 }
